58386048f659be2fe60e1b23e8a1f3328ababbb6
FRANCISCO JOSÉ <FRANCISCO JOS@192.168.207.2>
2022-08-31 13:00:39 -0500

    ultimo

--- PDSGrupal/src/com/grafica/principalPrueba.java
+++ PDSGrupal/src/com/grafica/principalPrueba.java
@@ -1,9 +1,10 @@
 package com.grafica;
 
 public class principalPrueba {
-
+private static int n1=0;
 	public static void main(String[] args) {
 		
+		n1 +=1;
 		try {
 			Usuario_Datos window = new Usuario_Datos();
 				//hola 	
