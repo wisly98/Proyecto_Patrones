b7116e925408503de9cc7fdb48f637b1e7b8bd27
Usuario-PC <Usuario-PC@DESKTOP-Q0GLB12>
2022-08-31 10:13:16 -0500

    Otro mas

--- PDSGrupal/src/com/grafica/Carrito_Pagar.java
+++ PDSGrupal/src/com/grafica/Carrito_Pagar.java
@@ -2,6 +2,13 @@ package com.grafica;
 
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.wb.swt.SWTResourceManager;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
 
 public class Carrito_Pagar {
 
@@ -26,6 +33,43 @@ public class Carrito_Pagar {
 		Shell shell = new Shell();
 		shell.setSize(450, 300);
 		shell.setText("SWT Application");
+		
+		Composite composite = new Composite(shell, SWT.NONE);
+		composite.setBounds(0, 0, 435, 261);
+		
+		Label efectivo = new Label(composite, SWT.NONE);
+		efectivo.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseDown(MouseEvent e) {
+				
+			}
+		});
+		efectivo.setImage(SWTResourceManager.getImage(Carrito_Pagar.class, "/com/imagenes/paypal.jpg"));
+		efectivo.setBounds(29, 83, 85, 78);
+		
+		Label transferencia = new Label(composite, SWT.NONE);
+		transferencia.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseDown(MouseEvent e) {
+			}
+		});
+		transferencia.setImage(SWTResourceManager.getImage(Carrito_Pagar.class, "/com/imagenes/transferencia.png"));
+		transferencia.setBounds(173, 83, 85, 78);
+		
+		Label tarjeta_debito = new Label(composite, SWT.NONE);
+		tarjeta_debito.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseDown(MouseEvent e) {
+				
+			}
+		});
+		tarjeta_debito.setImage(SWTResourceManager.getImage(Carrito_Pagar.class, "/com/imagenes/tarjeta.png"));
+		tarjeta_debito.setBounds(316, 83, 85, 78);
+		
+		Button btnRegresar = new Button(composite, SWT.NONE);
+		btnRegresar.setFont(SWTResourceManager.getFont("Segoe UI", 10, SWT.NORMAL));
+		btnRegresar.setBounds(173, 214, 75, 25);
+		btnRegresar.setText("Regresar");
 
 		shell.open();
 		shell.layout();
@@ -35,5 +79,4 @@ public class Carrito_Pagar {
 			}
 		}
 	}
-
 }
